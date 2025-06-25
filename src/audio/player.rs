3c8675d8292546039e477b21b4e742c8a2726551
erslie <kbyk0416@gmail.com>
2025-06-25 21:41:36 +0900

    chore player

--- src/audio/player.rs
+++ src/audio/player.rs
@@ -1,5 +1,4 @@
 
-
 use rodio::{OutputStream, Sink, Decoder};
 use std::fs::File;
 use std::io::BufReader;
@@ -46,9 +45,35 @@ impl AudioPlayer {
         let path = Path::new(path_str);
 
         if !path.exists() {
-            return Err(FerriaError::IoError(format!("File not found: {}", path_str)));
+                return Err(std::io::Error::new(std::io::ErrorKind::NotFound,format!("File not found: {}", path_str)).into());
         }
+
+        let file = File::open(path)?;
+
+        Ok(())
+        
     }
 }
 
+#[cfg(test)]
+mod test {
+    use crate::{audio::player::AudioPlayer, error::FerriaError};
 
+
+    #[test]
+    fn test_Audio_new() {
+        assert!(AudioPlayer::new().is_ok());
+    }
+
+    #[test]
+    fn test_Audio_play() {
+
+        let audio = AudioPlayer::new().unwrap();
+
+        let a = AudioPlayer::play(&audio, "a");
+
+        assert!(a.is_ok());
+
+    }
+
+} 
